2bcd8a9d6f37e01c176564947bd6101f5be32423
oracle <oracle@bigdatalite.localdomain>
2018-09-14 02:39:32 -0400

    modified purchase

--- purchase.sql
+++ purchase.sql
@@ -1,11 +1,11 @@
 DROP TABLE purchase;
 CREATE TABLE purchase (
   tx_date       VARCHAR2(255) DEFAULT NULL
-, cif           VARCHAR2(255) DEFAULT NULL
+, cif           NUMBER
 , source_of_tx  VARCHAR2(255) DEFAULT NULL
 , currency_code VARCHAR2(255) DEFAULT NULL
 , tx_id         VARCHAR2(255) DEFAULT NULL
-, amount        VARCHAR2(255) DEFAULT NULL
+, amount        NUMBER
 , merchant      VARCHAR2(255) DEFAULT NULL
 );
 EXIT
